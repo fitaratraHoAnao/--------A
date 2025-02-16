e9acedfebb5ee74e475808975c90fc7373c39dc8
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-16 22:08:02 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,25 +1,11 @@
-document.getElementById("getButton1").addEventListener("click", async () => {
-    const apiUrl = "/hello?question=Hello";
-    afficherReponse(apiUrl, "response1");
+document.getElementById("getButton1").addEventListener("click", () => {
+    window.location.href = "/hello?question=Hello";
 });
 
-document.getElementById("getButton2").addEventListener("click", async () => {
-    const apiUrl = "/bienvenue?question=Bienvenue";
-    afficherReponse(apiUrl, "response2");
+document.getElementById("getButton2").addEventListener("click", () => {
+    window.location.href = "/bienvenue?question=Bienvenue";
 });
 
-document.getElementById("getButton3").addEventListener("click", async () => {
-    const apiUrl = "/llama?question=bonjour";
-    afficherReponse(apiUrl, "response3");
+document.getElementById("getButton3").addEventListener("click", () => {
+    window.location.href = "/llama?question=Bonjour";
 });
-
-async function afficherReponse(apiUrl, elementId) {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        document.getElementById(elementId).innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-    } catch (error) {
-        console.error("Erreur lors de la récupération des données :", error);
-        document.getElementById(elementId).innerText = "Erreur lors du chargement des données.";
-    }
-}
