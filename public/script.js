cd91ed00037930fccc89e2fb44f3c3401b805e3c
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-16 20:40:45 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,3 @@
-document.getElementById('getButton').addEventListener('click', async () => {
-    const baseUrl = window.location.origin; // URL actuelle du site
-    const response = await fetch(`${baseUrl}/hello?question=Hello`);
-    const data = await response.json();
-    document.getElementById('response').textContent = "Réponse : " + data.response;
+document.getElementById("getButton").addEventListener("click", () => {
+    window.location.href = "/hello?question=Hello";
 });
