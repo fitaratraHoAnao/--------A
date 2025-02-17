fdd15823c9312a7c36dcf76c7b2c360077fd206f
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-17 12:26:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const helloRoute = require('./api/hello');
 const bienvenuRoute = require('./api/bienvenu');
 const llamaRoute = require('./api/llama');
+const deepseekRoute = require('./api/deepseek');
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/hello', helloRoute);
 app.use('/bienvenu', bienvenuRoute);
 app.use('/llama', llamaRoute); // Inclure la route Llama
-
+app.use('/deepseek', deepseekRoute);
 // Démarrer le serveur
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
