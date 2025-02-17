834e162ad89adf4f4afcac132e11e21b6b3e4ef3
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-17 13:04:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helloRoute = require('./api/hello');
 const bienvenuRoute = require('./api/bienvenu');
 const llamaRoute = require('./api/llama');
 const deepseekRoute = require('./api/deepseek');
+const deepseekqwenRoute = require('./api/deepseek-qwen');
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,6 +19,8 @@ app.use('/hello', helloRoute);
 app.use('/bienvenu', bienvenuRoute);
 app.use('/llama', llamaRoute); // Inclure la route Llama
 app.use('/deepseek', deepseekRoute);
+app.use('/deepseek', deepseekqwenRoute);
+
 // Démarrer le serveur
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
