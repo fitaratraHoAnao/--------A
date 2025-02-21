d4c6a1d2551b4a7c49891d928a07334f49c83a39
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-21 07:11:21 +0300

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const router = express.Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-router.get('/', (req, res) => {
-    const question = req.query.question || "Hello";
-    res.json({ response: "Hello World" });
+
+
+router.get('/date', async (req, res) => {
+  // Récupérer la variable 'heure' depuis le paramètre de la requête, par défaut 'madagascar'
+  const location = (req.query.heure || 'madagascar').toLowerCase();
+  const url = `https://www.timeanddate.com/time/zone/${location}`;
+
+  try {
+    // Effectuer la requête pour obtenir le contenu HTML de la page
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    // Extraire l'heure et la date à partir des éléments HTML correspondants
+    const heure = $('#ct').text().trim();
+    const date = $('#ctdat').text().trim();
+
+    // Retourner les informations en format JSON
+    res.json({
+      localisation: location.charAt(0).toUpperCase() + location.slice(1),
+      heure_actuelle: heure,
+      date_actuelle: date
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erreur lors de la récupération des informations" });
+  }
 });
 
-module.exports = router;
