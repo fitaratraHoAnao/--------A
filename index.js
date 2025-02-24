b29fc1c2460a6224c149c97541827ca1b241691c
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-24 21:27:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const conjugaisonRoute = require('./api/conjugaison');
 const quizRoute = require('./api/quiz');
 const translationRoute = require('./api/translation');
 const baibolyRoute = require('./api/baiboly');
-const tadiavinaRoute = require('./api/baiboly');
 const fihiranaRoute = require('./api/fihirana');
 const rechercheRoute = require('./api/synonyme');
 const antonymRoute = require('./api/antonyme');
@@ -23,22 +22,21 @@ const photoRoute = require('./api/photo');
 const gem29Route = require('./api/gemma');
 const mpanakantoRoute = require('./api/mpanakanto');
 const paroleRoute = require('./api/tononkira');
-const createRoute = require('./api/generate');
-const inboxRoute = require('./api/inbox'); 
+const generateRoute = require('./api/generate');
+const inboxRoute = require('./api/inbox');
 const genRoutes = require('./gen');
 const inboxRoutes = require('./boite');
 
-
-
-
 const PORT = process.env.PORT || 5000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
 app.use('/date', dateRoute);
 app.use('/bienvenu', bienvenuRoute);
-app.use('/llama', llamaRoute); 
+app.use('/llama', llamaRoute);
 app.use('/deepseek', deepseekRoute);
-app.use('/deepseek', deepseekqwenRoute);
+app.use('/deepseek-qwen', deepseekqwenRoute);
 app.use('/llama11', llama11Route);
 app.use('/qwen-coder', qwencoderRoute);
 app.use('/gemini', geminiRoute);
@@ -47,22 +45,23 @@ app.use('/quiz', quizRoute);
 app.use('/translation', translationRoute);
 app.use('/hira', fihiranaRoute);
 app.use('/recherche', rechercheRoute);
-app.use('/search', antonymRoute);
-app.use('/api', photoRoute);
+app.use('/antonyme', antonymRoute);
+app.use('/photo', photoRoute);
 app.use('/gem29', gem29Route);
 app.use('/mpanakanto', mpanakantoRoute);
 app.use('/parole', paroleRoute);
-app.use('/', baibolyRoute); 
-app.use('/', tadiavinaRoute);
-app.use('/create', genRoute);
+app.use('/baiboly', baibolyRoute);
+app.use('/create', generateRoute);
 app.use('/inbox', inboxRoute);
+app.use('/gen', genRoutes);
+app.use('/boite', inboxRoutes);
 
 app._router.stack.forEach((middleware) => {
-    if (middleware.route) {
-        console.log(middleware.route.path);
-    }
+    if (middleware.route) {
+        console.log(middleware.route.path);
+    }
 });
 
 app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
