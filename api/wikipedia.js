f5a320171fd184c84670c45764911e91e1bc8fc0
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-25 13:12:18 +0300

    Update wikipedia.js

--- api/wikipedia.js
+++ api/wikipedia.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+require('dotenv').config();
 
-const app = express();
-const PORT = 3000;
-
+const router = express.Router();
 // Route GET /wiki?recherche=Kolontsaina
-app.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
     const recherche = req.query.recherche;
 
     if (!recherche) {
@@ -48,7 +47,9 @@ app.get('/', async (req, res) => {
     }
 });
 
-// Démarrer le serveur
-app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+// Route 404
+router.use((req, res) => {
+    res.status(404).json({ error: "Route non trouvée" });
 });
+
+module.exports = router;
